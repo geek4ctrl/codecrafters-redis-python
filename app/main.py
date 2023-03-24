58c5fbc75a65d1a140e072f841a794de79b10f10
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-03-24 17:08:05 +0200

    Allow nothing

--- app/main.py
+++ app/main.py
@@ -11,6 +11,5 @@ def main():
     # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     # server_socket.accept() # wait for client
 
-
 if __name__ == "__main__":
     main()
